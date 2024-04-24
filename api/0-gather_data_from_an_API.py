f72f3a115a02200a123cc3689a34d490c43a40b4
Tunzale1 <tunzaleimanova03@mail.ru>
2024-04-24 11:48:49 +0000

    gathering data from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,24 +3,27 @@
 
 
 import requests
-import sys
+from sys import argv
+
+url_base = 'https://jsonplaceholder.typicode.com/users/'
 
 
-def gather_data():
-    """display on the standard output the employee TODO list progress """
-    user_id = sys.argv[1]
-    users_route = 'https://jsonplaceholder.typicode.com/users/{}'
-    todos_route = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
-    user = users_route.format(user_id)
-    todos = todos_route.format(user_id)
-    name = requests.get(user).json().get('name')
-    todos_request = requests.get(todos).json()
-    tasks = [task.get('title') for task in todos_request
-             if task.get('completed') is True]
-    print('Employee {} is done with tasks({}/{}):'.format(name,
-          len(tasks), len(todos_request)))
-    print('\n'.join('\t {}'.format(task) for task in tasks))
+def get_data():
+    """ This function get data of the placeholders API """
+    name = requests.get(url_base + argv[1]).json()
+    todos = requests.get(url_base + argv[1] + '/todos/').json()
+    count = 0
+    title = ""
+
+    for item in todos:
+        if item['completed'] is True:
+            title += "\t {}\n".format(item['title'])
+            count += 1
+
+    print("Employee {} is done with tasks({}/20):\n{}".format(name['name'],
+                                                              count, title),
+          end='')
 
 
-if __name__ == "__main__":
-    gather_data()
+if __name__ == '__main__':
+    get_data()
