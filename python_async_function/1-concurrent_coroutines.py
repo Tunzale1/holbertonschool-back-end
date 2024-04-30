ea45181ab8551030743810a079cbfc02c77cb526
Tunzale1 <tunzaleimanova03@mail.ru>
2024-04-30 19:12:46 +0000

    run attribute

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Let's execute multiple coroutines at the same time with async """
+import asyncio
+from typing import List
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Function that returns the list"""
+    listt = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    finish = [await task for task in asyncio.as_completed(listt)]
+    return finish
