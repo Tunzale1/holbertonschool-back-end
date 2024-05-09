58b88fac67f28923ae8a5e00513d2306d1291892
Tunzale1 <tunzaleimanova03@mail.ru>
2024-05-09 22:38:48 +0400

    pleasee

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,38 +1,72 @@
 #!/usr/bin/env python3
 """
-Main file
+Description: The goal here is that if between two queries, certain rows are
+             removed from the dataset, the user does not miss items from
+             dataset when changing page
 """
 
-Server = __import__('3-hypermedia_del_pagination').Server
-
-server = Server()
-
-server.indexed_dataset()
-
-try:
-    server.get_hyper_index(300000, 100)
-except AssertionError:
-    print("AssertionError raised when out of range")
+import csv
+import math
+from typing import List, Dict
 
 
-index = 3
-page_size = 2
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+    def __init__(self):
+        ''' Initialize instance. '''
+        self.__dataset = None
+        self.__indexed_dataset = None
 
-# 1- request first index
-res = server.get_hyper_index(index, page_size)
-print(res)
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-# 2- request next index
-print(server.get_hyper_index(res.get('next_index'), page_size))
+        return self.__dataset
 
-# 3- remove the first index
-del server._Server__indexed_dataset[res.get('index')]
-print("Nb items: {}".format(len(server._Server__indexed_dataset)))
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
-# 4- request again the initial index -> the first data retreives is not the same as the first request
-print(server.get_hyper_index(index, page_size))
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        ''' Return dict of pagination data.
+            Dict key/value pairs consist of the following:
+              index - the start index of the page
+              next_index - the start index of the next page
+              page_size
+              page_size - the number of items on the page
+              data - the data in the page itself '''
+        assert 0 <= index < len(self.dataset())
 
-# 5- request again initial next index -> same data page as the request 2-
-print(server.get_hyper_index(res.get('next_index'), page_size))
+        indexed_dataset = self.indexed_dataset()
+        indexed_page = {}
+
+        i = index
+        while (len(indexed_page) < page_size and i < len(self.dataset())):
+            if i in indexed_dataset:
+                indexed_page[i] = indexed_dataset[i]
+            i += 1
+
+        page = list(indexed_page.values())
+        page_indices = indexed_page.keys()
+
+        return {
+            'index': index,
+            'next_index': max(page_indices) + 1,
+            'page_size': len(page),
+            'data': page
+        }
